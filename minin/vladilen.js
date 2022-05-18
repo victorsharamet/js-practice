c2a04437d422cf28053045aaa39a9a44dbfe0525
victor.sharamet <sharamet@gmail.com>
2022-05-18 10:24:02 -0400

    facade commit

--- minin/vladilen.js
+++ minin/vladilen.js
@@ -1146,11 +1146,48 @@ console.log(s2.url)
 // 41:26
 
 // 7. facade
+class Complaints {
+	constructor() {
+		this.complaints = []
+	}
 
+	reply(complaint) {}
 
+	add(complaint) {
+		this.complaints.push(complaint)
+		return this.reply(complaint)
+	}
+}
 
+class ProductComplaints extends Complaints {
+	reply({id, customer, details}) {
+		return `Product: ${id}: ${customer} (${details})`
+	}
+}
 
+class SericeComplaints extends Complaints {
+	reply({id, customer, details}) {
+		return `Service: ${id}: ${customer} (${details})`
+	}
+}
 
+class ComplaintRegistry {
+	register(customer, type, details) {
+		const id = Date.now()
+		let ComplaintRegistry
 
+		if (type === 'service') {
+			complaint = new ServiceComplaints()
+		} else {
+			complaint = new ProductComplaints()
+		}
 
+		return complaint.add({id, customer, details}))
+	}
+}
 
+const registry = new ComplaintRegistry()
+
+console.log(registry.register('Vladilen', 'service', 'недоступен'))
+console.log(registry.register('Elena', 'product', 'вылазит ошибка'))
+// 50:37
