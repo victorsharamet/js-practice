ad93a2312d45a4aee35d1076860178f0f29397e5
victor.sharamet <sharamet@gmail.com>
2022-05-21 12:54:39 -0400

    command pattern commit

--- minin/vladilen.js
+++ minin/vladilen.js
@@ -1277,3 +1277,38 @@ console.log(sum1.add(8).add(10).add(1).add(9).sum)
 
 const sum2 = new MySum(0)
 console.log(sum2.add(1).add(2).add(3).sum)
+
+// 11. Command
+// Redux является характерным примером
+
+class MyMath {
+	constructor(initialValue = 0) {
+		this.num = initialValue
+	}
+
+	square() {
+		return this.num ** 2
+	}
+
+	cube() {
+		return this.num ** 3
+	}
+}
+
+class Command {
+	constructor(subject) {
+		this.subject = subject
+		this.commandsExecuted = []
+	}
+
+	execute() {
+		this.commandsExecuted.push(command)
+		return this.subject[command]()
+	}
+}
+
+const x = new Command (new MyMath(2))
+console.log(x.execute('square'))
+console.log(x.execute('cube'))
+console.log(x.commandsExecuted)
+// 01:12:00
