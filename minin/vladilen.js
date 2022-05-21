29a8d36b2d7d7fe8043523eccbc77e1a37c0cce4
victor.sharamet <sharamet@gmail.com>
2022-05-21 15:35:33 -0400

    observer design pattern commit

--- minin/vladilen.js
+++ minin/vladilen.js
@@ -1420,3 +1420,69 @@ vlad.send('Hello!', lena)
 lena.send('Hello hello!', vlad)
 igor.send('Vsem privet')
 // 1:25:58
+
+// Observer OR Publisher Subscriber OR Despatcher OR Listener
+// Формирует зависимости типа "один ко многим" - one to many dependencies
+// У нас есть объект, у которого мы можем стриггерить вызов изменений
+// Все объекты, подписанные на эти изменения, получают эти обновления и делают свой функционал
+class Subject {
+	constructor() {
+		this.observers = []
+	}
+
+	subscribe(observer) {
+		this.observers.push(observer)
+	}
+
+	unsubscribe(observer) {
+		this.observers = this.observers.filter(obs => obs !== observer)
+	}
+
+	fire(action) {
+		this.observers.forEach(observer => {
+			observer.update(action)
+		})
+	}
+}
+
+class Observer {
+	constructor(state = 1) {
+		this.state = state
+		this.initialState = state
+	}
+
+	update(action) {
+		switch (action.type) {
+			case 'INCREMENT':
+				this.state = ++this.state
+				break
+			case 'DECREMENT':
+				this.state = --this.state
+				break
+			case 'ADD':
+				this.state += action.payload
+				break
+			default: 
+				this.state = this.initialState
+		}
+	}
+}
+
+const stream$ = new Subject()
+
+const obs1 = new Observer()
+const obs2 = new Observer(42)
+
+stream$.subscribe(obs1)
+stream$.subscribe(obs2)
+
+stream$.fire({type: 'INCREMENT'})
+stream$.fire({type: 'INCREMENT'})
+stream$.fire({type: 'DECREMENT'})
+stream$.fire({type: 'ADD'})
+
+console.log(obs1.state)
+console.log(obs2.state)
+// 1:36:12
+
+// 
