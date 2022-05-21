a3d46d7d385d36e9d451da9412058d173ed46be8
victor.sharamet <sharamet@gmail.com>
2022-05-21 13:09:52 -0400

    iterator pattern commit

--- minin/vladilen.js
+++ minin/vladilen.js
@@ -1312,3 +1312,57 @@ console.log(x.execute('square'))
 console.log(x.execute('cube'))
 console.log(x.commandsExecuted)
 // 01:12:00
+
+// 12. Iterator
+class MyIterator {
+	constructor(data) {
+		this.index = 0
+		this.data = data
+	}
+
+	[Symbol.iterator]() {
+		return {
+			next: () => {
+				if (this.index < this.data.length) {
+					return {
+						value: this.data[this.index++],
+						done: false
+					}
+				} else {
+					this.index = 0
+					return {
+						done: true,
+						value: void 0
+					}
+				}
+			}
+		}
+	}
+}
+
+function* generator(collection) {
+	let index = 0
+
+	while (index < collection.length) {
+		yield collection[index++]
+	}
+}
+
+const iterator = new MyIterator(['This', 'is', 'iterator'])
+const gen = generator(['This', 'is', 'iterator'])
+
+for (const val of iterator) {
+	console.log('Value: ', val)
+}
+
+for (const val of gen) {
+	console.log('Value: ', val)
+}
+
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value) // undefined
+// 1:18:25
+
+
