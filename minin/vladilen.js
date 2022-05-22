74e4980b2a325b3f1d8a687c0271af6aac9100ad
victor.sharamet <sharamet@gmail.com>
2022-05-22 16:53:22 -0400

    template design pattern commit

--- minin/vladilen.js
+++ minin/vladilen.js
@@ -1598,3 +1598,49 @@ const commute = new Commute()
 console.log(commute.travel(new Taxi()))
 console.log(commute.travel(new Bus()))
 console.log(commute.travel(new Car()))
+
+// 17. Template
+class Employee {
+	constructor(name, salary) {
+		this.name = name
+		this.salary = salary
+	}
+
+	responsibilities() {}
+
+	work() {
+		return `${this.name} выполняет ${this.responsibilities}`
+	}
+
+	getPaid() {
+		return `${this.name} имеет ЗП ${this.salary}`
+	}
+}
+
+class Developer extends Employee {
+	constructor(name, salary) {
+		super(name, salary)
+	}
+
+	responsibilities() {
+		return `процесс создания программ`
+	}
+}
+
+class Tester extends Employee {
+	constructor(name, salary) {
+		super(name, salary)
+	}
+
+	responsibilities() {
+		return `процесс тестирования`
+	}
+}
+
+const dev = new Developer('Мистер Владилен', 100500)
+console.log(dev.getPaid())
+console.log(dev.work())
+
+const tester = new Tester('Виктория', 90000)
+console.log(tester.getPaid())
+console.log(tester.work())
