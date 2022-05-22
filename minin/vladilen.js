dc27d3c54545cc39f0184d6419c69920f8d5f75b
victor.sharamet <sharamet@gmail.com>
2022-05-22 16:43:07 -0400

    strategy design pattern

--- minin/vladilen.js
+++ minin/vladilen.js
@@ -1558,3 +1558,43 @@ traffic.change()
 console.log(traffic.sign())
 traffic.change()
 // 1:43:38
+
+// 16. Strategy
+class Vehicle {
+	travelTime() {
+		return this.timeTaken
+	}
+}
+
+class Bus extends Vehicle {
+	constructor() {
+		super()
+		this.timeTaken = 10
+	}
+}
+
+class Taxi extends Vehicle {
+	constructor() {
+		super()
+		this.timeTaken = 5
+	}
+}
+
+class Car extends Vehicle {
+	constructor() {
+		super()
+		this.timeTaken = 3
+	}
+}
+
+class Comute {
+	travel(transport) {
+		return transport.travelTime()
+	}
+}
+
+const commute = new Commute()
+
+console.log(commute.travel(new Taxi()))
+console.log(commute.travel(new Bus()))
+console.log(commute.travel(new Car()))
