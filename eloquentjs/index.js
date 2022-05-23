448d6191b5976f4e5fe540e6189d7e5874fa93ef
victor.sharamet <sharamet@gmail.com>
2022-05-24 01:09:45 +0300

    add: eloquent javascript book samples

---
+++ eloquentjs/index.js
@@ -0,0 +1,5 @@
+let theNumber = Number(promt("Pick a number"));
+if (!Number.isNaN(theNumber)) {
+	console.log("Your number is the square root of " + theNumber * theNumber);
+} // If you enter 'Parrot' no output is shown
+
